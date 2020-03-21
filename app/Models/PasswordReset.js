cf4c0a867f72a03300e80a0a38af34acea059691
luanBrandao <luanbrandao4@gmail.com>
2020-03-21 02:47:17 -0300

    Password Reset Model

---
+++ app/Models/PasswordReset.js
@@ -0,0 +1,30 @@
+'use strict'
+// adonis make:model PasswordReset
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class PasswordReset extends Model {
+
+  static boot(){
+    super.boot()
+
+    // implementa diretamenta a lógica no model
+    this.addHook('beforeCreate', async model => {
+      model.token = await str_random(25)
+      const expires_at = new Date()
+      now.setMinutes(expires_at.getMinutes() + 30)
+      model.expires_at = expires_at
+
+    })
+  }
+
+  // trata antes de salvar e apresetar os dados
+  // formata os valores para o padrão do MYSQL
+  static get dates(){
+    return ['created_at','updated_at','expires_at']
+  }
+
+
+}
+
+module.exports = PasswordReset
