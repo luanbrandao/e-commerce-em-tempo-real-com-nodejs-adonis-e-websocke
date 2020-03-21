9ed523958fba4a865ee6920b45a623fe99c0312b
luanBrandao <luanbrandao4@gmail.com>
2020-03-21 03:16:10 -0300

    Image Model

---
+++ app/Models/Image.js
@@ -0,0 +1,19 @@
+'use strict'
+// adonis make:model Image
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+const Env = use('Env')
+
+class Image extends Model {
+
+  // chama os metodos
+  static get computed(){
+    return ['url']
+  }
+  getUrl({path})  {
+    return `${Env.get('APP_URL')}/imagens/${path}`
+  }
+
+}
+
+module.exports = Image
