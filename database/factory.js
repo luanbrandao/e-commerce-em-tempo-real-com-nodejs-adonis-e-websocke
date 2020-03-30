5f85f5c16913baca23db6a9433687167291640fd
luanBrandao <luanbrandao4@gmail.com>
2020-03-30 18:41:04 -0300

    Factory - Blueprint dos Models

--- database/factory.js
+++ database/factory.js
@@ -12,7 +12,32 @@
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
-// const Factory = use('Factory')
+const Factory = use('Factory')
+
+
+Factory.blueprint('App/Models/User', (faker) => {
+  return {
+    username: faker.first(),
+    surname: faker.last(),
+    email: faker.email({ domain: 'gmail.com.br' }),
+    password: 'adsfasdf'
+  }
+})
+
+Factory.blueprint('App/Models/Category', (faker) => {
+  return {
+    title: faker.country({ full:true }),
+    description: faker.sentence()
+  }
+})
+
+Factory.blueprint('App/Models/Product', (faker) => {
+  return {
+    name: faker.animal(),
+    description: faker.sentence(),
+    price: faker.floating({ min: 0, max: 1000, fixed: 2 })
+  }
+})
 
 // Factory.blueprint('App/Models/User', (faker) => {
 //   return {
