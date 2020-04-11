ef0aa793246e9aec07055233b84f4823b52760e2
luanbrandao <luanbrandao4@gmail.com>
2020-04-11 01:02:53 -0300

    Helpers -  Gerenciando Uploads Únicos

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -1,5 +1,6 @@
 
 const crypto = use('crypto')
+const Helpers = use('Helpers')
 
 /**
  * @param { int } length - O tamanho da string que vocÇe quer gear
@@ -18,12 +19,35 @@ const crypto = use('crypto')
 
     string = buffer
       .toString('base64')
-      .replace(/^[a-zA-Z0-9]/g,'')
+      .replace(/[^a-zA-Z0-9]/g,'')
       .substr(0,size)
   }
 
   return string
  }
+
+
+ /**
+  * Move um único arquivo para o caminho especificado, se nenhum for especificado então
+  * o caminho 'public/upload' será usado
+  * @param { File } file p arquivoa ser gerenciado
+  * @param { string } path o caminho para onde o arquivo deve sermovido
+ */
+
+ const manage_single_upload = async ( file, path = null ) =>{
+   path = path ? path : Helpers.publicPath('uploads')
+   // gera um nome aleatório
+   const random_name = await str_random(30)
+   let filename = `${new Date().getTime()}-${random_name}.${file.subtype}`
+
+   //renomeia o arquivo e move ele para opath
+   await file.move(path, {
+     name: filename,
+    })
+
+    return file
+ }
+
 
  module.exports = {
     str_random
