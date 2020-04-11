11217b44db6a4a76dc523189ba88dd21477d3690
luanbrandao <luanbrandao4@gmail.com>
2020-04-11 01:11:16 -0300

    Helpers - Gerenciando Uploads Múltiplos

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -30,7 +30,7 @@ const Helpers = use('Helpers')
  /**
   * Move um único arquivo para o caminho especificado, se nenhum for especificado então
   * o caminho 'public/upload' será usado
-  * @param { File } file p arquivoa ser gerenciado
+  * @param { File } file o arquivoa ser gerenciado
   * @param { string } path o caminho para onde o arquivo deve sermovido
  */
 
@@ -46,6 +46,48 @@ const Helpers = use('Helpers')
     })
 
     return file
+ }
+
+ /**
+  * Move multiplos único arquivos para o caminho especificado, se nenhum for especificado então
+  * o caminho 'public/upload' será usado
+  * @param { FileJas } fileJar os arquivoa a serem gerenciado
+  * @param { string } path o caminho para onde os arquivos deve ser movido
+  * @return { Onject<FileJar> }
+ */
+
+ const manage_mutiple_uploads = async ( fileJar, path = null ) =>{
+
+  path = path ? path : Helpers.publicPath('uploads')
+
+  let successes = [],
+   erros = []
+
+   await Promise.all(fileJar.files.map( async file => {
+
+    // gera um nome aleatório
+    const random_name = await str_random(30)
+    let filename = `${new Date().getTime()}-${random_name}.${file.subtype}`
+
+    // move o arquivo
+
+   await file.move(path, {
+    name: filename,
+   })
+
+   //verificamos se moveu mesmo
+
+   if( file.moved() ) {
+    successes.push(file)
+   } else {
+     erros.push(file.error())
+   }
+
+   })
+  )
+
+  return { successes, erros }
+
  }
 
 
