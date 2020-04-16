8600112e56a19cb6a9a8943468875b7fff0290bd
luanbrandao <luanbrandao4@gmail.com>
2020-04-16 00:54:51 -0300

    Regras de Aplicação de Desconto

--- app/Services/Order/OrderService.js
+++ app/Services/Order/OrderService.js
@@ -1,5 +1,5 @@
 'use strict'
-
+const Database = use('Database')
 class OrderSerevice {
 
   constructor( model , trx = false) {
@@ -41,9 +41,77 @@ class OrderSerevice {
 
   }
 
+  async canApplyDiscount( counpon ) {
+    const couponProducts = await Database
+    .from('coupon_products')
+    .where(coupon_id , coupon.id)
+    .puck('product_id')
+    // retorna um array no formato: [1,2,3,...]
+
+    // associado a algum cliente
+    const couponClients = await Database
+    .from('coupon_user')
+    .where(coupon_id, coupon.id)
+    .puck('user_id')
+
+    // veriicar se o coupon não está associado a produtos e clientes específicos
+    if( Array.isArray(couponProducts) && couponProducts.length < 1 && Array.isArray(couponClients) && couponClients < 1 ) {
+      //  Caso não estaja associado a cliente ou produto específico, é de uso livre
+      return true
+    }
+
+    let isAssociatedToProducts, isAssociatedToClients = false
+
+    if( Array.isArray(couponProducts) && couponProducts > 0 ) {
+      isAssociatedToProducts = true;
+    }
+
+    if( Array.isArray(couponClients) && couponClients > 0 ) {
+      isAssociatedToClients = true;
+    }
+
+    const productsMatch  = await Database
+    .from('order_items')
+    .where('order_id' , this.model.id)
+    .whereIn('product_id', couponProducts)
+    .puck('product_id')
+
+    // caso de uso 1 - O cupom está associado a clientes e produtos
+    if( isAssociatedToClients && isAssociatedToProducts ) {
+      // find = filter
+      const clientMatch = couponClients.find(
+        client => client == this.model.user_id
+      )
 
 
+      if( clientMatch && Array.isArray(productsMatch) && productsMatch.length > 0) {
+        return true
+      }
 
+    }
+
+    // caso de uso 2- O cupom está apenas associado aapenas a produtos
+    if(  isAssociatedToProducts && Array.isArray(productsMatch) && productsMatch.length > 0) {
+      return true;
+    }
+    // caso de uso 3- O cupom está apenas associado a 1 ou mais clientes ( e nenhum produto)
+    if(  isAssociatedToClients && Array.isArray(couponClients) && couponClients.length > 0) {
+      const match = couponClients.find( client => client === this.model.user_id )
+      if( match ) {
+        return true;
+      }
+    }
+
+    /**
+     caso nenhuma das verificaçoes acima deem positivas
+     então o cupom está associado a clientes ou produtos ou os dois
+     porém nenhum dos produtos deste pedido está elegível ao desconto
+     e o cliente que fez a compra também não poderá utilizar este couponClients
+    */
+
+     return false
+
+  }
 }
 
 module.exports = OrderSerevice
